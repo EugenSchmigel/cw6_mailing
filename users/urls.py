272b699dd129b271dd46d09de1c075dedb7c80d1
eugen <eugenschmigel@gmail.com>
2023-12-30 17:37:36 +0100

    added csu.py

--- users/urls.py
+++ users/urls.py
@@ -1,16 +1,15 @@
-from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
 from users.apps import UsersConfig
-from users.views import RegisterView, ProfileView, verification_user, password_recovery
+from users.views import LoginView, LogoutView, RegisterView, UserUpdateView
 
 app_name = UsersConfig.name
 
+
 urlpatterns = [
-    path('', LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('register/', RegisterView.as_view(), name='register'),
-    path('success_register/<str:register_uuid>/', verification_user, name='success_register'),
-    path('password_recovery', password_recovery, name='password_recovery'),
-    path('profile/', ProfileView.as_view(), name='profile'),
+    path('profile/', UserUpdateView.as_view(), name='profile'),
+    # path('profile/genpassword/', generate_new_password, name='generate_new_password'),
 ]
